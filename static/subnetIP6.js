1e80ad16e09ddd4f8dca9d068b24d66543ea9232
UDUAK AKPADOK <WilliesGit>
2025-10-29 21:19:07 +0000

    Code clean up

--- static/subnetIP6.js
+++ static/subnetIP6.js
@@ -20,25 +20,25 @@ if (form && tableBody) {
         const subnetMaskValue = subnetMask.value.trim();
         const hostsValue = hostsInput.value.trim();
 
-        // Basic validation: ensure fields aren't empty
-        // Basic validation: ensure fields aren't empty
+        const hint_text =  document.querySelector('.hint')
+     
 
-        if(!ipAddress && !subnetMaskValue && !hostsValue){
-           const hint_text =  document.querySelector('.hint')
-           hint_text.style.color = 'red';
-           hint_text.style.border = '1px solid #f5a5a5';
-           hint_text.style.borderRadius = '3px';
-           hint_text.style.backgroundColor = '#ebc8d0ff';
-           hint_text.style.padding = '8px 10px';
-           return;
+        
+        // Basic validation: ensure fields aren't empty
+        if (!ipAddress && !subnetMaskValue) {
+            if (hint_text) {
+                hint_text.classList.add('hint_warning');
+                hint_text.classList.remove('hint_reset');
+            }
+            return;
+        } else {
+            if (hint_text) {
+                hint_text.classList.remove('hint_warning');
+                hint_text.classList.add('hint_reset');
+            }
         }
-        else{
-            const hint_text =  document.querySelector('.hint')
-            hint_text.style.color = '#475569';
-            hint_text.style.border = 'none';
-            hint_text.style.backgroundColor = 'transparent';
-            hint_text.classList.remove('red')
-        }
+
+        
         
         if (!ipAddress) {
             ipInput.classList.add('input-error');
